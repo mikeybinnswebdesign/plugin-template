d3b17ee8919d01bac5a0471e758de0b0966c2ceb
Mikey Binns <38146638+mikeybinnswebdesign@users.noreply.github.com>
2021-11-11 05:07:44 +0000

    Add custom tax

--- src/functions/add-taxonomies.php
+++
@@ -1,2 +0,0 @@
-<?php
-namespace %%PLUGIN_NAMESPACE%%\add_taxonomies;
---
+++ src/functions/register-custom-taxonomies.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This section allows you to easily add a page to the CMS that is required for the website.
+ *
+ * An example of this would be a log in page. You can use the helper function provided below to easily add pages and even add sub-pages to those pages.
+ *
+ * @package %%PLUGIN_NAMESPACE%%
+ */
+
+namespace %%PLUGIN_NAMESPACE%%\add_custom_tax;
+
+/*
+function custom_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Taxonomies', 'Taxonomy General Name', 'fit-for-reuse' ),
+		'singular_name'              => _x( 'Taxonomy', 'Taxonomy Singular Name', 'fit-for-reuse' ),
+		'menu_name'                  => __( 'Taxonomy', 'fit-for-reuse' ),
+		'all_items'                  => __( 'All Items', 'fit-for-reuse' ),
+		'parent_item'                => __( 'Parent Item', 'fit-for-reuse' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'fit-for-reuse' ),
+		'new_item_name'              => __( 'New Item Name', 'fit-for-reuse' ),
+		'add_new_item'               => __( 'Add New Item', 'fit-for-reuse' ),
+		'edit_item'                  => __( 'Edit Item', 'fit-for-reuse' ),
+		'update_item'                => __( 'Update Item', 'fit-for-reuse' ),
+		'view_item'                  => __( 'View Item', 'fit-for-reuse' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'fit-for-reuse' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'fit-for-reuse' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'fit-for-reuse' ),
+		'popular_items'              => __( 'Popular Items', 'fit-for-reuse' ),
+		'search_items'               => __( 'Search Items', 'fit-for-reuse' ),
+		'not_found'                  => __( 'Not Found', 'fit-for-reuse' ),
+		'no_terms'                   => __( 'No items', 'fit-for-reuse' ),
+		'items_list'                 => __( 'Items list', 'fit-for-reuse' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'fit-for-reuse' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'taxonomy', array( 'post' ), $args );
+
+}
+add_action( 'init', __NAMESPACE__ . '\\custom_taxonomy', 0 );
+*/
